7e444350d28c6fe92d1fe898d7e76b1a645e8faf
RodolfoC <RodolfoC@DESKTOP-T8EVELQ>
2016-08-03 00:05:44 -0600

    Agregada clase robot, recorre el laberinto. Atributos posicion actual

---
+++ ProyectoLaberinto/src/Robot.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RodolfoC
+ */
+public class Robot {
+    
+    private int [] posicion;
+
+    public void setPosicion(int[] posicion) {
+        this.posicion = posicion;
+    }
+
+    public int[] getPosicion() {
+        return posicion;
+    }
+    
+    
+}
