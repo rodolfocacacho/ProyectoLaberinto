6d29c797d1f0f983c748fafe189e75bbc3aacc66
Fredy Espana 15060 <fredy.espc@gmail.com>
2016-09-06 20:19:47 -0600

    Add files via upload

--- CodigoRobot.c
+++ CodigoRobot.c
@@ -1,9 +1,22 @@
+//Universidad Del Valle de Guatemala
+//Algoritmos y Estructuras de Datos
+//Segundo Ciclo 2016
+//                             PROYECTO 1                                  
+//Integrantes:  Diego Soler
+//              Fredy Espana
+//              Rodolfo Cacacho
+//              Rene Olivet
+
+
+
 #include "simpletools.h"                      // Include simpletools header
 #include "abdrive.h"                          // Include abdrive header
 #include "ping.h"                             // Include ping header
 
-int turn; //Variable para indicar al robot que decision tomar
-int num;
+int turn; 
+int num;  
+int anterior; 
+
 //Estructura del stack
 struct stack{
     int stk[100];
@@ -13,53 +26,86 @@ struct stack{
 typedef struct stack ST;
 ST s;
 
-void push(int);
+
+void push(int);  //Metodos del Stack
 int pop(void);
 
-int main()                                    // main function
+int main()                                    // MAIN
 {
    
+   //Llamada a funcion recursiva
    recursive();   
 
  
  }
 
+
+//Funcion recursiva
 void recursive(){
+  
+
   while(1){
-    
+ 
+  //Mientras haya obstaculo lejos, siga avanzando   
   while(ping_cm(8)>= 10){
        pause(5);
        num= rand() % 3;
        push(num);
        drive_speed(60,60);
-   
-    
-  } 
-   
-    
-
-    if (RIR() == 0){
-      
-        
-       turnLeft();
-       recursive(); 
        
-            
-     }
+       
+       //Si hay camino enfrente y a los lados
+       if ((LIR()==1 && RIR()==1)){
      
+               //Si el giro anterior fue a la izquierda, gira a la derecha
+               if (anterior == 1){
+               pause(600);
+               turnRight();
+               drive_goto(40,40);
+               pause(500);
+               recursive(); //Llamada recursiva
+               }
+               
+               //Si el giro anterior fue a la derecha, gira a la izquierda
+
+                if (anterior == 2){
+                pause(600);
+                turnLeft();
+                drive_goto(40,40);
+                pause(500); 
+                recursive();
+               } 
+                  
+           }            
+       
+     }       
+            
+  
+     
+     //Si hay obstaculo a la izquierda, gire a la derecha
+
      if (LIR() == 0) {
        turnRight();
        recursive(); 
       }
- 
-      
-         
       
       
+      //Si hay obstaculo enfrente y a los lados, regrese
+      if(ping_cm(8)<10 && (RIR()==0 && LIR()==0)){
+       
+       turnBack();
+       recursive();
+        
+      } 
       
+      
+      
+      //Si no, solo hay obstaculo enfrente y gire aleatoriamente a la izquierda o a la derecha
       else{
        
        turn=pop();
+       anterior=turn;
+       drive_speed(0,0);
   
     if (turn == 1){
        turnLeft();
@@ -77,67 +123,62 @@ void recursive(){
         
   }
   
+ 
+  
  }  
             
-    
+
+//Infrarrojo derecho    
 int RIR(){
-   freqout(1, 1, 27000);
+   freqout(1, 1, 35000);
    return input(2);
 } 
 
+//Infrarrojo izquierdo
 int LIR(){
-   freqout(11, 1, 27000);
+   freqout(11, 1, 35000);
    return input(10);
 }   
 
-void interseccion(){
-    //revisar ambos lados para ver si hay lugares a donde cruzar
-}
-  
+ 
+ 
+//Giro a la derecha  
 void turnRight(){ 
 
   pause(250);
   drive_speed(0,0);
-  drive_goto(28,-25); //gira a la izquierda 
+  drive_goto(28,-25); //Gira a la derecha
   pause(500);
   drive_goto(5,5);
+  anterior=2;
   return;  
 
 }
 
+
+//Giro a la izquierda
 void turnLeft(){
 
   pause(250);
   drive_speed(0,0);
-  drive_goto(-28,25); //gira a la izquierda 
+  drive_goto(-28,25); //Gira a la izquierda 
   pause(500);
   drive_goto(5,5);
+  anterior=1;
   return;  
 
 }
 
-//Cuando tiene pared en ambos lados y enfrente
-void turnBack(){
-    puese(250);
-    drive_speed(0,0);
-    drive_goto(-25,25);
-    drive_goto(-25,25); 
-    pause(500);
-    drive_goto(5,5);
-    return; 
-}
-    
-}
-
-
-
+//Push al stack
 void push(int a){
     s.top = s.top+1;
     s.stk[s.top]= a;
   }
-  
+
+//Pop al stack  
 int pop(){
     int a = s.stk[s.top];
     s.top = s.top - 1;
     return a;
   }
+  
